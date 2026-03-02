8b87903a92ae8226432c15cfd618f8fc4094b3c8
Shamem-Miah <shamemmiah2@gmail.com>
2026-03-02 19:13:04 +0600

    debugged

--- frontend/src/context/axiosInstance.js
+++ frontend/src/context/axiosInstance.js
@@ -9,10 +9,10 @@ export const axiosInstance = axios.create({
 });
 
 // Add response interceptor to handle token refresh if needed
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // Adjust if using cookies
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token"); // Adjust if using cookies
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
